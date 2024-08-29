0e262d7536761eda035ec0edba64390a4e46fd04
nadhif1867 <0106.nadhif@gmail.com>
2024-08-29 18:53:38 +0700

    Pembuatan KTP

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,10 +4,62 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>PWL 2024</title>
+        <title>KTP</title>
     </head>
     <body>
-        <h2>Pemrograman Web Lanjut 2024</h2>
-        <h3>Semangat Belajar Laravel 10</h3>
+        <table border="0" cellpadding="10" cellspacing="0" align="center" width="1080">
+            <tr>
+                <td colspan="5" align="center">PROVINSI JAWA TIMUR</td>
+            </tr>
+            <tr>
+                <td colspan="5" align="center">KOTA MALANG</td>
+            </tr>
+            <tr>
+                <td colspan="1">NIK</td>
+                <td colspan="2">: 3574878956514782</td>
+            </tr>
+            <tr>
+                <td colspan="1">Nama</td>
+                <td colspan="2">: Moch. Nadhif Alkautsar</td>
+            </tr>
+            <tr>
+                <td colspan="1">Jenis Kelamin</td>
+                <td colspan="2">: LAKI-LAKI</td>
+                <td colspan="4">Gol. Darah</td>
+                <td colspan="5">: -</td>
+            </tr>
+            <tr>
+                <td colspan="1">Alamat</td>
+                <td colspan="2">: Jl. Soekarno Hatta</td>
+            </tr>
+            <tr>
+                <td colspan="2">    RT/RW</td>
+                <td colspan="2">: 006/010</td>
+            </tr>
+            <tr>
+                <td colspan="2">    Kel/Desa</td>
+                <td colspan="2">: LOWOKWARU</td>
+            </tr>
+            <tr>
+                <td colspan="2">&nbps; Kecamatan</td>
+                <td colspan="2">: LOWOKWARU</td>
+            </tr>
+            <tr>
+                <td colspan="1">Agama</td>
+                <td colspan="2">: ISLAM</td>
+            </tr>
+            <tr>
+                <td colspan="1">Status Perkawinan</td>
+                <td colspan="2">: Belum Kawin</td>
+            </tr>
+            <tr>
+                <td colspan="1">Kewarganegaraan</td>
+                <td colspan="2">: WNI</td>
+            </tr>
+            <tr>
+                <td colspan="1">Berlaku Hingga</td>
+                <td colspan="2">: SEUMUR HIDUP</td>
+            </tr>
+        </table>
     </body>
 </html>
