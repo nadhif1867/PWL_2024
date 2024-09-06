2bf8bc51fa99297e992926f478c5bb7c9052a925
nadhif1867 <0106.nadhif@gmail.com>
2024-09-06 19:21:03 +0700

    Praktikum - Jobsheet 2

---
+++ resources/views/ktp.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>KTP</title>
+    </head>
+    <body>
+        <table border="0" cellpadding="10" cellspacing="0" align="center" width="1080">
+            <tr>
+                <td colspan="5" align="center">PROVINSI JAWA TIMUR</td>
+            </tr>
+            <tr>
+                <td colspan="5" align="center">KOTA MALANG</td>
+            </tr>
+            <tr>
+                <td colspan="1">NIK</td>
+                <td colspan="2">: 3574878956514782</td>
+            </tr>
+            <tr>
+                <td colspan="1">Nama</td>
+                <td colspan="2">: Moch. Nadhif Alkautsar</td>
+            </tr>
+            <tr>
+                <td colspan="1">Jenis Kelamin</td>
+                <td colspan="2">: LAKI-LAKI</td>
+                <td colspan="4">Gol. Darah</td>
+                <td colspan="5">: -</td>
+            </tr>
+            <tr>
+                <td colspan="1">Alamat</td>
+                <td colspan="2">: Jl. Soekarno Hatta</td>
+            </tr>
+            <tr>
+                <td colspan="2">    RT/RW</td>
+                <td colspan="2">: 006/010</td>
+            </tr>
+            <tr>
+                <td colspan="2">    Kel/Desa</td>
+                <td colspan="2">: LOWOKWARU</td>
+            </tr>
+            <tr>
+                <td colspan="2">&nbps; Kecamatan</td>
+                <td colspan="2">: LOWOKWARU</td>
+            </tr>
+            <tr>
+                <td colspan="1">Agama</td>
+                <td colspan="2">: ISLAM</td>
+            </tr>
+            <tr>
+                <td colspan="1">Status Perkawinan</td>
+                <td colspan="2">: Belum Kawin</td>
+            </tr>
+            <tr>
+                <td colspan="1">Kewarganegaraan</td>
+                <td colspan="2">: WNI</td>
+            </tr>
+            <tr>
+                <td colspan="1">Berlaku Hingga</td>
+                <td colspan="2">: SEUMUR HIDUP</td>
+            </tr>
+        </table>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,16 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\UserProfileController;
+use App\Http\Controllers\WelcomeController;
+use Illuminate\Foundation\Console\AboutCommand;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +24,118 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Praktikum 1
+// Route::get('/hello', function(){
+//     return 'Hello World';
+// });
+
+// Route::get('/world', function(){
+//     return 'World';
+// });
+
+// Route::get('/ucapan', function(){
+//     return 'Selamat Datang';
+// });
+
+// Route::get('/about', function(){
+//     return '2241760067 Moch. Nadhif Alkautsar';
+// });
+
+// Route::get('/user/{Nadhif}', function($Nadhif){
+//     return 'Nama saya '. $Nadhif;
+// });
+
+// Route::get('/posts/{post}/comments/{comment}', function
+// ($postId, $commentId) {
+//  return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+// });
+
+// Route::get('/articles/{id}', function($id){
+//     return 'Halaman artikel dengan ID '.$id;
+// });
+
+// Route::get('/user/{name?}', function($name=null){
+//     return 'Nama saya '.$name;
+// });
+
+// Route::get('/user/{name?}', function($name='John'){
+//     return 'Nama saya '.$name;
+// });
+
+// // route name
+// Route::get('/user/profile', function(){
+//     //
+// })->name('profile');
+
+// Route::get(
+//     'user/profile',
+//     [UserProfileController::class, 'show']
+// )->name('profile');
+
+
+// route group
+// Route::middleware(['first', 'second'])->group(function(){
+//     Route::get('/', function(){
+//         // uses first and second minddleware
+//     });
+// Route::get('/user/profile', function(){
+//         // uses first and second middleware
+//     });
+// });
+// Route::domain('{account}.example.com')->group(function(){
+//     Route::get('/user/{id}', function($account, $id){
+//         //
+//     });
+// });
+// Route::middleware('auth')->group(function(){
+//     Route::get('/user', [UserController::class, 'index']);
+//     Route::get('/post', [PostController::class, 'index']);
+//     Route::get('/event', [EventController::class, 'index']);
+// });
+
+// //redirect
+// Route::redirect('/here', '/there');
+
+// //view
+// Route::view('/welcome', 'welcome');
+// Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
+
+// Praktikum 2ss
+// controller
+
+// Route::get('/hello', [WelcomeController::class,'hello']);
+
+// Page Controller
+Route::get('/', [PageController::class,'index']);
+Route::get('/about', [PageController::class, 'about']);
+Route::get('/articles/{id}', [PageController::class, 'articles']);
+
+// Home, About, Article Controller
+Route::get('/', [HomeController::class,'index']);
+Route::get('/about', [AboutController::class, 'about']);
+Route::get('/articles/{id}', [ArticleController::class, 'articles']);
+
+// Resources 
+Route::resource('photos', PhotoController::class);
+
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+]);
+
+// Praktikum 3
+// Route::get('/greeting', function(){
+//     return view('hello', ['name' => 'Nadhif']);
+// });
+
+// Route::get('/greeting', function(){
+//     return view('blog.hello', ['name' => 'Nadhif']);
+// });
+
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
